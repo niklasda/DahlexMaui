3479a67a7f6f8578210488a897f641b850ef5442
Niklas <nd@sql8r.net>
2022-09-23 14:49:42 +0200

    new empty

---
+++ DahlexApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DahlexApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DahlexApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DahlexApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
